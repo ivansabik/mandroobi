a346258346f10af0f53cf21531fc888abf76814e
Ivan Rodriguez <ivan@britecore.com>
2017-04-30 13:19:48 -0500

    update fk to use model not table ref

--- mandroobi/models/misc.py
+++ mandroobi/models/misc.py
@@ -1,7 +1,10 @@
+from __future__ import absolute_import
+
 from sqlalchemy import Column, ForeignKey, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy_mixins import ActiveRecordMixin
 
+from .dimensions import Scenario
 from ..db import session
 
 Base = declarative_base()
@@ -11,7 +14,7 @@ class ExchangeRate(Base, ActiveRecordMixin):
     __tablename__ = 'exchange_rates'
 
     iso_code = Column('iso_code', String(length=3), primary_key=True)
-    scenario_id = Column('id', String(length=50), ForeignKey('scenarios.id'),)
+    scenario_id = Column('id', String(length=50), ForeignKey(Scenario.id),)
     type = Column('type', String(length=3))
 
 
