1db7cb776750419e006c9f4c08619f91b1ebb452
Ivan Rodriguez <ivan@britecore.com>
2017-04-30 23:02:49 -0500

    move add test dims to utils

--- tests/test_models_metrics.py
+++ tests/test_models_metrics.py
@@ -1,22 +1,9 @@
 from __future__ import absolute_import
+
 import pytest
 
 from .utils import create_test_db  # noqa
-from mandroobi.models.dimensions import Account, AccountingPeriod, BusinessUnit, Currency, Driver, Scenario
 from mandroobi.models.metrics import ClosingBalance, Metric
-
-
-def _add_test_dimensions():
-    Account.create(id='10000', description='Cash & Banks', parent_id=None, type='asset')
-    Account.create(id='20000', description='Accounts Payable', parent_id=None, type='liability')
-    Account.create(id='30000', description='Common Stock', parent_id=None, type='equity')
-    Account.create(id='40000', description='Sales', parent_id=None, type='income')
-    Account.create(id='50000', description='Cost of Sales', parent_id=None, type='expense')
-    AccountingPeriod.create(id='20170101', description='Jan 2017', parent_id=None, month=1, quarter=1, year=2017)
-    BusinessUnit.create(id='TESTINO_LLC', description='Testino, LLC', parent_id=None, local_currency='USD')
-    Currency.create(id='USD', description='US Dollar', parent_id=None)
-    Driver.create(id='NO_DRIVER', description='No Planning Driver', parent_id=None)
-    Scenario.create(id='ACTUAL', description='Actual', parent_id=None)
 
 
 def test_metric():
@@ -36,7 +23,6 @@ def test_closing_balance():
 
 @pytest.mark.usefixtures('create_test_db')
 def test_closing_balance_inc_lia_equ_reverse_sign():
-    _add_test_dimensions()
     closing_balance = ClosingBalance.create(
         account_id='20000',
         accounting_period_id='20170101',
--- tests/utils.py
+++ tests/utils.py
@@ -2,6 +2,7 @@ import pytest
 
 from mandroobi.db import engine
 from mandroobi.models import dimensions, metrics, misc
+from mandroobi.models.dimensions import Account, AccountingPeriod, BusinessUnit, Currency, Driver, Scenario
 
 
 @pytest.fixture()
@@ -14,3 +15,14 @@ def create_test_db():
 
     misc.Base.metadata.drop_all(engine)
     misc.Base.metadata.create_all(engine)
+
+    Account.create(id='10000', description='Cash & Banks', parent_id=None, type='asset')
+    Account.create(id='20000', description='Accounts Payable', parent_id=None, type='liability')
+    Account.create(id='30000', description='Common Stock', parent_id=None, type='equity')
+    Account.create(id='40000', description='Sales', parent_id=None, type='income')
+    Account.create(id='50000', description='Cost of Sales', parent_id=None, type='expense')
+    AccountingPeriod.create(id='20170101', description='Jan 2017', parent_id=None, month=1, quarter=1, year=2017)
+    BusinessUnit.create(id='TESTINO_LLC', description='Testino, LLC', parent_id=None, local_currency='USD')
+    Currency.create(id='USD', description='US Dollar', parent_id=None)
+    Driver.create(id='NO_DRIVER', description='No Planning Driver', parent_id=None)
+    Scenario.create(id='ACTUAL', description='Actual', parent_id=None)
